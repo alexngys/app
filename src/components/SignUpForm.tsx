81a54aaa743800f782e5a680453b6b1fa0f4b884
alex ng <114757545+alexngys@users.noreply.github.com>
2023-04-03 15:46:32 +0100

    a

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,114 +1,129 @@
 import React, { useState } from "react";
-import {db} from '../firebase'
+import { db } from "../firebase";
 import { setDoc, doc } from "firebase/firestore";
 
 const style = {
-    form: "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4",
-    title:"block text-gray-700 text-lg font-bold mb-2",
-    text: "block text-gray-700 text-sm font-bold mb-2",
-    username: "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
-    dropdown: "block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline",
-    button: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mx-auto"
-}
+  form: "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4",
+  title: "block text-gray-700 text-lg font-bold mb-2",
+  text: "block text-gray-700 text-sm font-bold mb-2",
+  username:
+    "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+  dropdown:
+    "block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline",
+  button:
+    "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mx-auto",
+};
 
 const defaultFormData = {
-    name:'',
-    dance:1,
-    freestyle:1,
-}
+  name: "",
+  dance: 1,
+  freestyle: 1,
+};
 
-const SignUpForm = () =>{
-    const [inputs, setInputs] = useState(defaultFormData);
-    const {name,dance,freestyle} = inputs
-    const [formsuccess, setformsuccess] = useState('')
-    const [formerror, setformerror] = useState('')
+const SignUpForm = () => {
+  const [inputs, setInputs] = useState(defaultFormData);
+  const { name, dance, freestyle } = inputs;
+  const [formsuccess, setformsuccess] = useState("");
+  const [formerror, setformerror] = useState("");
 
-    const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-        setInputs((prevState) => ({
-            ...prevState,
-            [e.target.id]: e.target.value,
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
     }));
-    };
-    
-    const onClick = (e:React.ChangeEvent<HTMLSelectElement>) => {
-        setInputs((prevState) => ({
-            ...prevState,
-            [e.target.id]: +e.target.value,
-        }));
-    };
+  };
 
+  const onClick = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.id]: +e.target.value,
+    }));
+  };
 
-    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        setformsuccess('')
-        setformerror('')
-        e.preventDefault();
-        if (name===""){
-            setformerror('Enter a valid name')
-            return
-        }
-        if (name.length<4){
-            setformerror('Name must be longer than 4 characters')
-            return
-        }
-        let uppername = name[0].toUpperCase() + name.substring(1);
-        await setDoc(doc(db,'users',uppername), {
-            name: uppername,
-            dance: dance,
-            freestyle: freestyle,
-            total: dance+freestyle
-        })
-        
-        setInputs(defaultFormData);
-        setformsuccess(uppername)
-    };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setformsuccess("");
+    setformerror("");
+    e.preventDefault();
+    if (name === "") {
+      setformerror("Enter a valid name");
+      return;
+    }
+    if (name.length < 4) {
+      setformerror("Name must be longer than 4 characters");
+      return;
+    }
+    let uppername = name[0].toUpperCase() + name.substring(1);
+    await setDoc(doc(db, "users", uppername), {
+      name: uppername,
+      dance: dance,
+      freestyle: freestyle,
+      total: dance + freestyle,
+    });
 
+    setInputs(defaultFormData);
+    setformsuccess(uppername);
+  };
 
-
-    return(
-        <div>
-            <form className={style.form} onSubmit={onSubmit}>
-            <p className={style.title}>Welcome to Docks Games 3 sign in page</p>
-            {formsuccess && <p className={style.title}>Thank you {formsuccess} for signing in</p>}
-            <div className="mb-6">
-                <label className={style.text}>
-                Enter your full name
-                </label>
-                <input className={style.username} id="name" type="text" placeholder="Full Name" value={name} onChange={onChange}></input>
-                {formerror && <p className={style.text}>{formerror}</p>}
-            </div>
-            
-            <div className="mb-6">
-                <label className={style.text}>
-                What is your dance skill level?
-                </label>
-                <select className={style.dropdown} id='dance' value={dance} onChange={onClick}>
-                    <option value={1}>Beginner </option>
-                    <option value={2}>Intermediate </option>
-                    <option value={3}>Advanced</option>
-                    <option value={4}>Pro</option>
-                </select>
-            </div>
-
-            <div className="mb-6">
-                <label className={style.text}>
-                What is your freestyle skill level?
-                </label>
-                <select className={style.dropdown} id='freestyle' value={freestyle} onChange={onClick}>
-                    <option value={1}>Beginner</option>
-                    <option value={2}>Intermediate</option>
-                    <option value={3}>Advanced</option>
-                    <option value={4}>Pro</option>
-                </select>
-            </div>
-
-            <div className="flex items-center justify-between">
-                <button className={style.button} type="submit">
-                    Submit
-                </button>
-            </div>
-            </form>
+  return (
+    <div>
+      <form className={style.form} onSubmit={onSubmit}>
+        <p className={style.title}>Welcome to Docks Games 3 sign in page</p>
+        {formsuccess && (
+          <p className={style.title}>Thank you {formsuccess} for signing in</p>
+        )}
+        <div className="mb-6">
+          <label className={style.text}>Enter your full name</label>
+          <input
+            className={style.username}
+            id="name"
+            type="text"
+            placeholder="Full Name"
+            value={name}
+            onChange={onChange}
+          ></input>
+          {formerror && <p className={style.text}>{formerror}</p>}
         </div>
-    )
-}
 
-export default SignUpForm
+        <div className="mb-6">
+          <label className={style.text}>What is your dance skill level?</label>
+          <select
+            className={style.dropdown}
+            id="dance"
+            value={dance}
+            onChange={onClick}
+          >
+            <option value={1}>Beginner </option>
+            <option value={2}>Intermediate </option>
+            <option value={3}>Advanced</option>
+            <option value={4}>Pro</option>
+          </select>
+        </div>
+
+        <div className="mb-6">
+          <label className={style.text}>
+            What is your freestyle skill level?
+          </label>
+          <select
+            className={style.dropdown}
+            id="freestyle"
+            value={freestyle}
+            onChange={onClick}
+          >
+            <option value={1}>Beginner</option>
+            <option value={2}>Intermediate</option>
+            <option value={3}>Advanced</option>
+            <option value={4}>Pro</option>
+          </select>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <button className={style.button} type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/pages/Sorting.tsx
+++ src/pages/Sorting.tsx
@@ -1,119 +1,135 @@
-import React, { useEffect, useState } from 'react'
-import { collection, orderBy, query, getDocs } from "firebase/firestore"
-import {db} from '../firebase'
+import React, { useEffect, useState } from "react";
+import { collection, orderBy, query, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 const style = {
-    appContainer: 'max-w-[728px] mx-auto text-center',
-    text: " block text-gray-700 text-sm font-bold mb-2",
-    form: "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4",
-    dropdown: "block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline",
-    button: "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mx-auto",
-}
+  appContainer: "max-w-[728px] mx-auto text-center",
+  text: " block text-gray-700 text-sm font-bold mb-2",
+  form: "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4",
+  dropdown:
+    "block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline",
+  button:
+    "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mx-auto",
+  table: "shadow-lg bg-white py-2 table-fixed w-full",
+  thead: "text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 ",
+  th:"px-6 py-3",
+  td: "border px-4 py-4",
+  card: "w-auto mb-6 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700",
+};
 
 const defaultFormData = {
-    teams:2
-}
+  teams: 2,
+};
 
 const Sorting = () => {
-    class users{
-        name:string;
-        score:number;
-        constructor(name: string, score:number) {
-            this.name=name;
-            this.score=score;
-        }
+  class users {
+    name: string;
+    score: number;
+    constructor(name: string, score: number) {
+      this.name = name;
+      this.score = score;
     }
-    const [inputs, setInputs] = useState(defaultFormData);
-    const {teams} = inputs
-    const [userdata,setuserdata]=useState<users[]>([])
+  }
+  const [inputs, setInputs] = useState(defaultFormData);
+  const { teams } = inputs;
+  const [userdata, setuserdata] = useState<users[]>([]);
 
-    useEffect(() => {
-        const getitems = async () =>{
-            let userlist: users[] = []
-            const q = query(collection(db, "users"), orderBy('total','desc'))
-            const querySnapshot = await getDocs(q);
-            querySnapshot.forEach((doc) => {
-                const tempdata = doc.data()
-                userlist.push({name:tempdata.name,score:tempdata.total});
-             
-            });
-            setuserdata(userlist)
-        }
-        getitems();
-    },[]);
-    
-
-    const onClick = (e:React.ChangeEvent<HTMLSelectElement>) => {
-        setInputs((prevState) => ({
-            ...prevState,
-            [e.target.id]: +e.target.value,
-        }));
+  useEffect(() => {
+    const getitems = async () => {
+      let userlist: users[] = [];
+      const q = query(collection(db, "users"), orderBy("total", "desc"));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        const tempdata = doc.data();
+        userlist.push({ name: tempdata.name, score: tempdata.total });
+      });
+      setuserdata(userlist);
     };
-    
-    const [tables,settables] = useState<string[][]>([['']])
+    getitems();
+  }, []);
 
-    const onSubmit = (e:React.FormEvent<HTMLFormElement>) =>{
-        e.preventDefault();
-        var data: string[][] = [] 
-        var counter = []
-        
-        for (var i=0; i<teams; i++){
-            data.push([])
-            counter.push(0)
-        }
-        for (var j=0; j<userdata.length; j++){
-            let minimum = Math.min(...counter)
-            let minimumindex = counter.indexOf(minimum)
-            data[minimumindex].push(userdata[j].name)
-            counter[minimumindex] += userdata[j].score
-        } 
-        console.log(data,counter)
-        settables(data)
+  const onClick = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.id]: +e.target.value,
+    }));
+  };
+
+  const [tables, settables] = useState<string[][]>([[]]);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    var data: string[][] = [];
+    var counter = [];
+
+    for (var i = 0; i < teams; i++) {
+      data.push([]);
+      counter.push(0);
     }
-    
-    return(
-        <div className={style.appContainer}>
-        <p className={style.text}>Number of players: {userdata.length}</p>
-        <form className={style.form} onSubmit={onSubmit}>
-            <div className="mb-6">
-                <label className={style.text}>
-                How many teams?
-                </label>
-                <select className={style.dropdown} id='teams' value={teams} onChange={onClick}>
-                    <option value={2}>2</option>
-                    <option value={3}>3</option>
-                    <option value={4}>4</option>
-                    <option value={5}>5</option>
-                    <option value={6}>6</option>
-                    <option value={7}>7</option>
-                    <option value={8}>8</option>
-                </select>
-            </div>
+    for (var j = 0; j < userdata.length; j++) {
+      let minimum = Math.min(...counter);
+      let minimumindex = counter.indexOf(minimum);
+      data[minimumindex].push(userdata[j].name);
+      counter[minimumindex] += userdata[j].score;
+    }
+    settables(data);
+  };
 
-            <div className="flex items-center justify-between">
-                <button className={style.button} type="submit">
-                    Submit
-                </button>
-            </div>
-
-        </form>
-        {tables.map((item,index) => {
-            return(
-                <div>
-                    <header>Team {index+1}</header>
-                    {item.map((player) => {
-                        return(
-                            <table>
-                                <tr>{player}</tr>
-                            </table>
-                        )
-                    })}
-                </div>
-            )
-    })}
+  return (
+    <div className={style.appContainer}>
+      <p className={style.text}>Number of players: {userdata.length}</p>
+      <form className={style.form} onSubmit={onSubmit}>
+        <div className="mb-6">
+          <label className={style.text}>How many teams?</label>
+          <select
+            className={style.dropdown}
+            id="teams"
+            value={teams}
+            onChange={onClick}
+          >
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+            <option value={6}>6</option>
+            <option value={7}>7</option>
+            <option value={8}>8</option>
+          </select>
         </div>
 
-    );
+        <div className="flex items-center justify-between">
+          <button className={style.button} type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+
+      <div className="">
+        {tables.map((item, index) => {
+          return (
+            <div className={style.card}>
+              <table className={style.table}>
+                <thead className={style.thead}>
+                  <tr>
+                    <th className={style.th}>Team {index + 1}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {item.map((player) => {
+                    return (
+                      <tr>
+                        <td className={style.td}>{player}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
-  
+
 export default Sorting;
